7018b42de8221669c69a995e181e92b6c9258891
MICHAEL ANGELO GRUMEZ OPLO <angeloece@gmail.com>
2023-07-10 11:53:37 +0800

    routes ang pages

---
+++ src/pages/ChatsPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page padding>
+    Chat Page
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,9 +2,20 @@
 const routes = [
   {
     path: '/',
+    component: () => import('layouts/GuestLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/LoginPage.vue') },
+      { path: '', component: () => import('pages/RegistrationPage.vue') },
+    ]
+  },
+  {
+    path: '/auth',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: 'Patient', component: () => import('pages/PatientPage.vue') },
+      { path: 'Appointments', component: () => import('pages/AppointmentPage.vue') },
+      { path: 'Chat', component: () => import('pages/ChatsPage.vue') },
+      { path: 'Consultations', component: () => import('pages/ConsultationsPage.vue') },
     ]
   },
 
